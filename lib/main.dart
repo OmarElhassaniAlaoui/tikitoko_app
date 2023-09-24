d26e342d967e883db675f7510a5e39c733505df9
omar <el.ell.elassani@gmail.com>
2023-09-24 16:26:50 +0100

    setup project

---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:tikto_app/src/tikto_app.dart';
+
+void main() {
+  runApp(const TiktoApp());
+}
+
+
---
+++ lib/src/tikto_app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:tikto_app/src/presentation/template/pages/template.dart'; 
+
+class TiktoApp extends StatelessWidget {
+  const TiktoApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const Template(), 
+    );
+  }
+}   
+
+
