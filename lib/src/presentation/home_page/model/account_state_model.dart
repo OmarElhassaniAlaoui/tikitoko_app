2e522a246f898eac21fca9227ac5cf23b1d0d66e
omar <el.ell.elassani@gmail.com>
2023-09-26 15:21:28 +0100

    finish routing pages

--- lib/src/presentation/home_page/model/account_state_model.dart
+++ lib/src/presentation/home_page/model/account_state_model.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:tikto_app/src/app/routes/app_pages.dart';
 import 'package:tikto_app/src/app/themes/app_palette.dart';
 
 class AccountStatModel {
@@ -7,44 +8,37 @@ class AccountStatModel {
   final String routeName;
   final Color? color;
 
-  AccountStatModel({
-    required this.title,
-    required this.value,
-    required this.routeName,
-    this.color  
-  });
+  AccountStatModel(
+      {required this.title,
+      required this.value,
+      required this.routeName,
+      this.color});
 
   static List<AccountStatModel> getAccountStatus() {
     List<AccountStatModel> accountStatList = [];
     accountStatList.add(AccountStatModel(
-      title: 'Followers',
-      value: '100',
-      // routeName: AppPages.followersList,
-      routeName: "AppPages.followersList",
-      color: AppPalette.babyBlueLightBg
-
-    ));  
+        title: 'Followers',
+        value: '100',
+        // routeName: AppPages.followersList,
+        routeName: AppPages.followersList,
+        color: AppPalette.babyBlueLightBg));
     accountStatList.add(AccountStatModel(
-      title: 'Following',
-      value: '100',
-      // routeName: AppPages.followingList,
-      routeName: 'AppPages.followingList',
-      color: AppPalette.violetLightBg
-    )); 
+        title: 'Following',
+        value: '100',
+        // routeName: AppPages.followingList,
+        routeName: AppPages.followingList,
+        color: AppPalette.violetLightBg));
     accountStatList.add(AccountStatModel(
-      title: 'Vedios',
-      value: '100',
-      routeName: '/repos-list',
-      color: AppPalette.peachLightBg
-    )); 
+        title: 'Vedios',
+        value: '100',
+        routeName: '/repos-list',
+        color: AppPalette.peachLightBg));
     accountStatList.add(AccountStatModel(
-      title: 'Likes',
-      value: '100',
-      routeName: '/gists-list',
-      color: AppPalette.waterLightBg
-    ));
+        title: 'Likes',
+        value: '100',
+        routeName: '/gists-list',
+        color: AppPalette.waterLightBg));
 
     return accountStatList;
   }
-
 }
