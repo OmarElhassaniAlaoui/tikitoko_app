25da97ba50a7d0498bf4d164ba1b46310820f38b
omar <el.ell.elassani@gmail.com>
2023-10-03 20:37:58 +0100

    add device preview

--- lib/src/presentation/home_page/widgets/account_state_widget.dart
+++ lib/src/presentation/home_page/widgets/account_state_widget.dart
@@ -20,65 +20,59 @@ class AccountStatWidget extends StatelessWidget {
       if (userStateController.userStateList.isEmpty) {
         return const Center(child: CircularProgressIndicator());
       } else {
-        return SizedBox(
-          height: 400,
-          child: GridView.builder(
-            primary: false,
-            padding: const EdgeInsets.all(20),
-            shrinkWrap: true,
-            physics: const NeverScrollableScrollPhysics(),
-            gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              mainAxisSpacing: 20,
-              crossAxisSpacing: 20,
-            ),
-            itemCount: homeController.accountStatList.length,
-            itemBuilder: (context, index) {
-              return GestureDetector(
-                onTap: () {
-                  Get.toNamed(homeController.accountStatList[index].routeName);
-                },
-                child: Container(
-                  padding: const EdgeInsets.all(20),
-                
-                  decoration: BoxDecoration(
-                    color: homeController.accountStatList[index].color,
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(8.0),
-                        child: SvgPicture.asset(
-                          homeController.accountStatList[index].icon,
-                          height: 40,
-                          width: 40,
-                        ),
-                      ),
-                      Text(
-                        homeController.accountStatList[index].title,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      const SizedBox(
-                        height: 10,
-                      ),
-                      Text(
-                        '${homeController.accountStatList[index].value}',
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              );
-            },
+        return GridView.builder(
+          shrinkWrap: true,
+          physics: const NeverScrollableScrollPhysics(),
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            mainAxisSpacing: 15,
+            crossAxisSpacing: 15,
           ),
+          itemCount: homeController.accountStatList.length,
+          itemBuilder: (context, index) {
+            return GestureDetector(
+              onTap: () {
+                Get.toNamed(homeController.accountStatList[index].routeName);
+              },
+              child: Container(
+                padding: const EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: homeController.accountStatList[index].color,
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: SvgPicture.asset(
+                        homeController.accountStatList[index].icon,
+                        height: 40,
+                        width: 40,
+                      ),
+                    ),
+                    Text(
+                      homeController.accountStatList[index].title,
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '${homeController.accountStatList[index].value}',
+                      style: const TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                  ],
+                ),
+              ),
+            );
+          },
         );
       }
     });
